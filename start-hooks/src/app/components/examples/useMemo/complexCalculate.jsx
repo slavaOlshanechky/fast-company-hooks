5cec7d2e47ff1e7349789e79a0e409eeba4d521c
slavaOlshanechky <olshanechky@gmail.com>
2022-02-16 22:12:29 +0200

    add useRef, useMemo

---
+++ start-hooks/src/app/components/examples/useMemo/complexCalculate.jsx
@@ -0,0 +1,54 @@
+import React, {useEffect, useState, useMemo} from "react";
+import CardWrapper from "../../common/Card";
+import SmallTitle from "../../common/typografy/smallTitle";
+
+function factorial(n) {
+    return n ? n * factorial(n - 1) : 1
+}
+
+function runFactorial(n) {
+    console.log("Run factorial")
+    return factorial(n)
+}
+
+const ComplexCalculateExample = () => {
+    const [value, setValue] = useState(100)
+    const [otherState, setOtherState] = useState(false)
+    const buttonColor = otherState ? "primary" : "secondary"
+    useEffect(() => {
+        console.log('render')
+    }, [buttonColor])
+    const fact = useMemo(() => runFactorial(value), [value])
+    return (
+        <>
+            <CardWrapper>
+                <SmallTitle>Кэширование сложных вычислений</SmallTitle>
+                <p>Value: {value}</p>
+                <p>Result factorial: {fact}</p>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={() => setValue(prevState => prevState + 10)}
+                >
+                    Increase
+                </button>
+                <button
+                    className="btn btn-primary mx-2"
+                    onClick={() => setValue(prevState => prevState - 10)}
+                >
+                    Decrease
+                </button>
+            </CardWrapper>
+            <CardWrapper>
+                <SmallTitle>Зависимость от сторонних setState</SmallTitle>
+                <button
+                    className={"btn ms-md-2 btn-" + buttonColor}
+                    onClick={() => setOtherState((prevState) => !prevState)}
+                >
+                    Change color
+                </button>
+            </CardWrapper>
+        </>
+    );
+};
+
+export default ComplexCalculateExample;
---
+++ start-hooks/src/app/components/examples/useRef/prevState.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useRef, useState} from "react";
+import CardWrapper from "../../common/Card";
+import SmallTitle from "../../common/typografy/smallTitle";
+
+const PrevStateExample = () => {
+    const prevState = useRef("")
+    const [otherState, setOtherState] = useState("false")
+
+    const toggleOtherState = () => {
+        setOtherState(prevState => prevState === "false" ? "true" : "false")
+    }
+    useEffect(() => {
+        prevState.current = otherState
+    }, [otherState])
+    return (<CardWrapper>
+            <SmallTitle>Предыдущее состояние</SmallTitle>
+            <p>prev State:{prevState.current}</p>
+            <p>Current State:{otherState}</p>
+            <button className="btn btn-primary" onClick={toggleOtherState}>
+                Toggle other state
+            </button>
+        </CardWrapper>
+
+    );
+};
+
+export default PrevStateExample;
